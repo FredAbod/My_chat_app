9f361ee0ef568b1e5d406c00ced82d6765f2d4d8
FredAbod <fredrickbolutife@gmail.com>
2022-09-09 19:04:57 +0100

    java format

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect("http://localhost:3400");
+const socket = io.connect("http://localhost:3400 || https://chat-app-ayqu.onrender.com");
 
 const message = document.getElementById("message"),
   user = document.getElementById("user"),
